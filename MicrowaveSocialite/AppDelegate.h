84742558d5a20dae81b2ea8e7934048f0be1023e
Spencer Whyte <spencerwhyte@dhcp-212-191.eduroam.carleton.ca>
2014-05-27 13:32:27 -0400

    Initial Commit

---
+++ MicrowaveSocialite/AppDelegate.h
@@ -0,0 +1,15 @@
+//
+//  AppDelegate.h
+//  MicrowaveSocialite
+//
+//  Created by Spencer Whyte on 2014-05-27.
+//  Copyright (c) 2014 ___FULLUSERNAME___. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+@end
---
+++ MicrowaveSocialite/main.m
@@ -0,0 +1,18 @@
+//
+//  main.m
+//  MicrowaveSocialite
+//
+//  Created by Spencer Whyte on 2014-05-27.
+//  Copyright (c) 2014 ___FULLUSERNAME___. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+#import "AppDelegate.h"
+
+int main(int argc, char * argv[])
+{
+    @autoreleasepool {
+        return UIApplicationMain(argc, argv, nil, NSStringFromClass([AppDelegate class]));
+    }
+}
